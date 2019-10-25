72e685a9ed449bf00fe1848851e2178af8ff2105
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2019-10-25 04:33:54 -0400

    another push

--- lib/narrabot/fabulous_fables.rb
+++ lib/narrabot/fabulous_fables.rb
@@ -44,28 +44,3 @@ end
 
 
 =end
-
-
-
-
-
-=begin
-
-    --- FABULOUS FABLES TELLS ALL ---
-
-  HALLO, HOLA, ALOHA, and KONICHIWA!!!  I'm Fabulous Fables.
-  I work for Narrabot and Lazy Librarian.
-    You'd think:
-    if Lazy Librarian was gonna go through all the trouble of making
-      Lazy Librarian Objects, then...
-    she'd at least go through the trouble of grabbing the text for each story.
-      Right?
-    SMH.  She can't handle it.
-    And then she blames ME for spending too much time with Nokogiri.
-    But, if she wants to blame anyone:
-      she oughta look in the mirror first.
-    That and... not to brag, but I'm kind of a big deal.
-    No one splits text like me.
-    ::SNAPS FINGERS::
-
-=end
--- lib/narrabot/scraper.rb
+++ lib/narrabot/scraper.rb
@@ -48,22 +48,3 @@ class Scraper
 
 end
 =end
-
-
-# Nasty No's Haiku about his Blade: Nokogiri
-#
-# Fine tooth saw, find find
-#   Whatever the heart desire
-# A P I?  Not I.
-
-# Nasty No's Haiku about Lazy Librarian
-#
-# a
-#   The scraper is, whatever
-# you need it to be.
-
-# Nasty No's Haiku about Fabulous Fables
-#
-# I'm an illusion
-#   require me for... am I
-# a method?  Use me.
